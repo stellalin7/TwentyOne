e1ed6514a789df2e995379f4bb92c3a69e6e7d64
Primo <primo@Primos-MacBook-Air.local>
2019-05-03 15:36:50 -0500

    test commit new dir

---
+++ cerealHowTo/src/cerealHowTo/cerealHowTo.java
@@ -0,0 +1,50 @@
+package cerealHowTo;
+
+public class cerealHowTo {
+
+	public static void main(String[] args) {
+		//fill null elements when re-supplied
+		
+		String ingredients [][] = {
+				{"cereal","pantry"},
+				{"milk",null},	
+		};
+		
+		String supplies [][] = {
+				{"bowl",null},
+				{"spoon","drawer"}	
+		};
+		
+		int i=0;
+		
+		do {
+			if (ingredients[i][1] != null) {
+				System.out.println(String.format("Get %s from %s", ingredients[i][0],ingredients[i][1]));
+				i++;
+			}else {
+				System.out.println("Need to buy "+ingredients[i][0]);
+				break;
+			}
+			if (i==ingredients.length) {
+				int j=0;
+				do {
+					if (supplies[j][1] != null) {
+						System.out.println(String.format("Get %s from %s", supplies[j][0],supplies[j][1]));
+						j++;
+					}else {
+						System.out.println("Need to wash "+supplies[j][0]);
+						break;
+					}
+					if (j==supplies.length) {
+						for (int y = 0; y<ingredients.length;y++) {
+							System.out.println(String.format("Pour %s into bowl",ingredients[y]));
+						}
+						System.out.println("Eat your cereal with a spoon");
+					}
+				}while(j<supplies.length);
+			}
+		}while(i<ingredients.length);
+		
+		
+	}
+}
