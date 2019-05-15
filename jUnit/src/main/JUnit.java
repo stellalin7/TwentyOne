b3f79ce43a717cb6fccf25fc79b7ceb693e38ad0
Primo <primo@Primos-MacBook-Air.local>
2019-05-15 10:11:29 -0500

    tournament add winning logic

--- jUnit/src/main/JUnit.java
+++ jUnit/src/main/JUnit.java
@@ -1,5 +1,9 @@
 package main;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.test;
+
 public class JUnit {
 
 }
