c672864bf1c1f0212508601e0c1fb0103e0a3811
Primo <primo@Primos-MacBook-Air.local>
2019-05-15 14:40:46 -0500

    random value generator test

--- twentyOne/src/twentyOne/TwentyOneTest.java
+++ twentyOne/src/twentyOne/TwentyOneTest.java
@@ -2,15 +2,37 @@ package twentyOne;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
 
 class TwentyOneTest {
-
+	
+	private static final long VALUES_TO_TEST = 1000;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+    public void testGetIndex() {
+        for (int i=0; i < VALUES_TO_TEST; i++) {
+        	assertTrue(TwentyOne.getIndex()<13);
+        }
+    }
+	
+	@Test
+	public void testDealerHand() {
+		for (int i=0; i < VALUES_TO_TEST; i++) {
+        	assertTrue(TwentyOne.dealerHand()<=21);
+        }
 	}
-
+	
+	@Test
+	public void testDealerNext() {
+		for (int i=0; i < VALUES_TO_TEST; i++) {
+			Random rand = new Random();
+			int randomBeginningScore = rand.nextInt(3);
+        	assertTrue(TwentyOne.dealerNext(randomBeginningScore)<=21);
+        }
+	}
+	
 	@Test
 	void testCompareHands() {
 		assertTrue(TwentyOne.compareHands(21, 21));
